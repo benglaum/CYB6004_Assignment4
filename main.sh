0e40feef3fe10ee15b6d8ae66b7640ec0143fdd5
benglaum <88817555+benglaum@users.noreply.github.com>
2022-02-25 20:40:38 +1000

    processing of data

--- main.sh
+++ main.sh
@@ -1,22 +1,48 @@
 #!/bin/bash
 
 #Colours
-R="\033[1;31m"
-G="\033[1;32m"
-Y="\033[1;33m"
-B="\033[1;34m"
-P="\033[1;35m"
+R="\033[31m"
+G="\033[32m"
+Y="\033[33m"
+B="\033[34m"
+P="\033[35m"
 N="\033[0m"
 
 #Variables
 SPACE=" "
 VERTICAL="|"
 
-#Downloads country information and stores it in a text file
-#wget -q "https://stats.cybergreen.net/country" -O countryStats.txt
+sedFormating() {
+    sed -i "s%$1%$2%g" $3
+}
+
+dataEditing() {
+    #Remove all the lines that don't contain the information.
+    awk '(/data-place/ || /data-score/ || (/style=/ && /align=/)) {print $0}' $1 > $2
+
+    #Remove all white space from front from each line.
+    sedFormating '^[ \t]*' '' $2
+
+    #Turn into a string to remove new lines.
+    string=$(cat $2)
+
+    #Add a new line so that each country is on a new line.
+    echo $string | awk '{gsub(/\<tr data-place="/,"\n")}1' > $2
+
+    #Getting to the correct formating.
+    sedFormating '"> <td class="score" title="' ';' $2
+    sedFormating '" data-score="' ';' $2
+    sedFormating '" > <span align="right" style="padding-right:15px">' ';' $2
+    sedFormating '</span> <td class="score" title="' ';' $2
+    sedFormating '"> <span align="right" style="padding-right:15px">' ';' $2
+    sedFormating '</span> <' '' $2
+    sed -i '1d' $2
+}
 
 #Runs the password check script.
 #./PasswordCheck.sh
+
+
 
 #If the password is correct then the if statement is executed.
 if [ $? -eq 0 ]; then
@@ -25,17 +51,19 @@ if [ $? -eq 0 ]; then
 	while [ 1 ]; do
 
 	    #Displays the menu
-        printf "$R%s$N\n" "Risks"
-        printf "%s\n" "1. Open Recursive DNS"
-        printf "%s\n" "2. Open NTP"
-        printf "%s\n" "3. Open SNMP"
-        printf "%s\n" "4. Open SSDP"
-        printf "%s\n" "5. Open Chargen"
-        printf "%s\n" "6. DDOS"
-        printf "%s\n" "7. Exit"
+        printf "\n$B%s$N\n\n" "Main Menu"
+        printf "$P%s$B%s$P%s$N\n\n" "Type " "'download'" " to download the lastest data."
+        printf "$P%s$B%s$P%s$N\n\n" "Type " "'exit'" " to exit."
+        printf "$P%s$N\n" "Type a number to investigate a Risk."
+        printf "    $B%s$P%s$N \n" "1. " "Open Recursive DNS"
+        printf "    $B%s$P%s$N\n" "2. " "Open NTP"
+        printf "    $B%s$P%s$N\n" "3. " "Open SNMP"
+        printf "    $B%s$P%s$N\n" "4. " "Open SSDP"
+        printf "    $B%s$P%s$N\n" "5. " "Open Chargen"
+        printf "    $B%s$P%s$N\n\n" "6. " "DDOS"
 
         #Asks the user to select the option.
-        read -p $'\e[33mSelect the number: \e[0m' selection
+        read -p $'\e[34mEnter: \e[0m' selection
 
         case "$selection" in
 
@@ -45,9 +73,16 @@ if [ $? -eq 0 ]; then
 	    "4") printf "4\n" ;;
 	    "5") printf "5\n" ;;
 	    "6") printf "6\n" ;;
-        "7") exit ;;
         "exit") exit ;;
-
+        "download")
+        printf "DOWNLOAD.." 
+        #Downloads country information and stores it in a text file
+        #wget -q "https://stats.cybergreen.net/country" -O countryStats.txt
+        
+        #function that does all the pre processing.
+        dataEdit countryStats.txt next.txt
+        ;;
+        
         esac
             printf "$R%s$N\n" "Not a valid option"
 	done
@@ -55,3 +90,5 @@ if [ $? -eq 0 ]; then
 else
 	exit
 fi
+
+
